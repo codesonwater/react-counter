24e5e4b48979998a53fd468d8dd8d6b0f2687c6f
Jeremiah Newman <jeremiah.n.a.newman@gmail.com>
2022-02-10 16:52:09 -0500

    Todd's Example

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import {useState} from 'react';
 import './App.css';
 
 function App() {
-  const[counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const [cars, setCars] = useState(['Audi', 'Tesla', 'Ford']);
+  const [person, setPerson] = useState({ name: 'Jeremiah', age: 31, shirt: 'tan' });
   return (
     <div className="App">
       <header className="App-header">
        <h1>Counter: {counter}</h1>
-       
+       <h2>Hello {person.name}</h2>
+       <h2>{cars.toString()}</h2>
+       <button onClick={() => setCars([...cars, 'Kia'])}>Add Kia</button>&nbsp;
        <button
        onClick={() => setCounter(counter -1)}> - </button>
        &nbsp; 
